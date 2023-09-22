379fada22e1bbb129ca903009df73f75562380b7
Devansh Akruvala <devanshakruvala@gmail.com>
2023-09-22 15:31:16 -0400

    Project Initialization

---
+++ src/test/java/com/soen6441/risk_game_u14/AppTest.java
@@ -0,0 +1,20 @@
+package com.soen6441.risk_game_u14;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
