8f8f7fc31dd972ab0c0955895b09d315da4cb1e4
Meshva Patel <stu.mdp@gmail.com>
2023-09-27 19:18:55 -0400

    Test Suite Added

---
+++ src/test/java/com/soen6441/risk_game_u14/controller/AllTests.java
@@ -0,0 +1,10 @@
+package com.soen6441.risk_game_u14.controller;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({MapControllerTest.class})
+public class AllTests {
+
+}
