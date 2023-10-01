1615e17f6473a1da5cded28ee4b7d0d63f5cd369
smridhi29 <smridhi68@gmail.com>
2023-10-01 15:13:16 -0400

    remov Neighbor added

--- src/main/java/com/soen6441/risk_game_u14/controller/MapController.java
+++ src/main/java/com/soen6441/risk_game_u14/controller/MapController.java
@@ -2,45 +2,42 @@ package com.soen6441.risk_game_u14.controller;
 
 import com.soen6441.risk_game_u14.model.Map;
 
-
-
 /**
  * 
  * 
- *@author Devansh Meshva Smridhi
+ * @author Devansh Meshva Smridhi
  * 
  */
 public class MapController {
 	private Map d_Map;
-	
+
 	public MapController(Map map) {
 		this.d_Map = map;
 	}
 
-	public String addContinentCommand(String p_Command){
+	public String addContinentCommand(String p_Command) {
 		String l_CommandSplit[] = p_Command.split(" ");
 		int l_CommandSplitLength = l_CommandSplit.length;
-		
+
 		if (l_CommandSplit[0].equalsIgnoreCase("editcontinent")) {
 			int l_WordIndex = 1;
 			while (l_WordIndex < l_CommandSplitLength) {
-				if (l_CommandSplit[l_WordIndex].equalsIgnoreCase("-add") && l_WordIndex+2<l_CommandSplitLength) {				
+				if (l_CommandSplit[l_WordIndex].equalsIgnoreCase("-add") && l_WordIndex + 2 < l_CommandSplitLength) {
 					try {
-						d_Map.addContinent(l_CommandSplit[l_WordIndex + 1], Integer.parseInt(l_CommandSplit[l_WordIndex + 2]));
+						d_Map.addContinent(l_CommandSplit[l_WordIndex + 1],
+								Integer.parseInt(l_CommandSplit[l_WordIndex + 2]));
 					} catch (NumberFormatException ne) {
 						return "Enter integer for Continent value";
-					} 
-					catch (Exception e) {
+					} catch (Exception e) {
 						return e.getMessage();
 					}
-					l_WordIndex+=3;
-				}
-				else if(l_CommandSplit[l_WordIndex].equalsIgnoreCase("-remove") && l_WordIndex+1<l_CommandSplitLength) {
+					l_WordIndex += 3;
+				} else if (l_CommandSplit[l_WordIndex].equalsIgnoreCase("-remove")
+						&& l_WordIndex + 1 < l_CommandSplitLength) {
 					System.out.println("Handle remove");
-					l_WordIndex+=2;
-				}
-				else {
-					//throw new Exception("Invalid Command");
+					l_WordIndex += 2;
+				} else {
+					// throw new Exception("Invalid Command");
 					return "Invalid Command";
 				}
 			}
@@ -57,19 +54,18 @@ public class MapController {
 		if (l_CommandSplit[0].equalsIgnoreCase("editcountry")) {
 			int l_WordIndex = 1;
 			while (l_WordIndex < l_CommandSplitLength) {
-				if (l_CommandSplit[l_WordIndex].equalsIgnoreCase("-add") && l_WordIndex+2<l_CommandSplitLength) {
+				if (l_CommandSplit[l_WordIndex].equalsIgnoreCase("-add") && l_WordIndex + 2 < l_CommandSplitLength) {
 					try {
-						d_Map.addCountries(l_CommandSplit[l_WordIndex+1], l_CommandSplit[l_WordIndex+2]);
+						d_Map.addCountries(l_CommandSplit[l_WordIndex + 1], l_CommandSplit[l_WordIndex + 2]);
 					} catch (Exception e) {
 						return e.getMessage();
 					}
-					l_WordIndex+=3;
-				}
-				else if(l_CommandSplit[l_WordIndex].equalsIgnoreCase("-remove") && l_WordIndex+1<l_CommandSplitLength) {
+					l_WordIndex += 3;
+				} else if (l_CommandSplit[l_WordIndex].equalsIgnoreCase("-remove")
+						&& l_WordIndex + 1 < l_CommandSplitLength) {
 					System.out.println("Handle remove");
-					l_WordIndex+=2;
-				}
-				else {
+					l_WordIndex += 2;
+				} else {
 					System.out.println("Invalid Command");
 					return "Invalid Command";
 				}
@@ -79,75 +75,75 @@ public class MapController {
 			return "Invalid Command";
 		}
 	}
-	
+
 	public String addNeighborsCommand(String p_Command) {
 		String l_CommandSplit[] = p_Command.split(" ");
 		int l_CommandSplitLength = l_CommandSplit.length;
 		if (l_CommandSplit[0].equalsIgnoreCase("editneighbor")) {
 			int l_WordIndex = 1;
 			while (l_WordIndex < l_CommandSplit.length) {
-				if (l_CommandSplit[l_WordIndex].equalsIgnoreCase("-add")  && l_WordIndex+2<l_CommandSplitLength) {
+				if (l_CommandSplit[l_WordIndex].equalsIgnoreCase("-add") && l_WordIndex + 2 < l_CommandSplitLength) {
 					try {
-						d_Map.addCountryNeighbour(l_CommandSplit[l_WordIndex+1], l_CommandSplit[l_WordIndex+2]);
+						d_Map.addCountryNeighbour(l_CommandSplit[l_WordIndex + 1], l_CommandSplit[l_WordIndex + 2]);
 					} catch (Exception e) {
 						return e.getMessage();
 					}
-					l_WordIndex+=3;
-				}
-				else if(l_CommandSplit[l_WordIndex].equalsIgnoreCase("-remove") && l_WordIndex+1<l_CommandSplitLength) {
-					l_WordIndex+=2;
-				}
-				else {
-					
-					//throw new Exception("Invalid Command");
+					l_WordIndex += 3;
+				} else if (l_CommandSplit[l_WordIndex].equalsIgnoreCase("-remove")
+						&& l_WordIndex + 2 < l_CommandSplitLength) {
+					try {
+						d_Map.removeNeighbor(l_CommandSplit[l_WordIndex + 1], l_CommandSplit[l_WordIndex + 2]);
+					} catch (Exception e) {
+						return e.getMessage();
+					}
+					l_WordIndex += 3;
+				} else {
 					return "Invalid Command";
 				}
 			}
-			return "Neighbors added succcessfully";
+			return "Neighbors command executed succcessfully";
 		} else {
 			return "Invalid Command";
-//			System.out.println("Invalid Command Country");
+			// System.out.println("Invalid Command Country");
 		}
 	}
 
-
-
 	public String removeCountryCommand(String p_Command) throws Exception {
-    
-    String l_CommandSplit[] = p_Command.split(" ");
-    int l_CommandSplitLength = l_CommandSplit.length;
 
-    // Check if the first word in the command is "editcountry" 
-    if (l_CommandSplit[0].equalsIgnoreCase("editcountry")) {
-        int l_WordIndex = 1;
+		String l_CommandSplit[] = p_Command.split(" ");
+		int l_CommandSplitLength = l_CommandSplit.length;
 
-        while (l_WordIndex < l_CommandSplitLength) {
-            // Check if the current word is "-remove" and there is at least one more word in the command
-            if (l_WordIndex + 1 < l_CommandSplitLength &&
-                l_CommandSplit[l_WordIndex].equalsIgnoreCase("-remove")) {
-                // Call method d_Map.removeCountry to remove a country
-                d_Map.removeCountry(l_CommandSplit[l_WordIndex + 1]);
+		// Check if the first word in the command is "editcountry"
+		if (l_CommandSplit[0].equalsIgnoreCase("editcountry")) {
+			int l_WordIndex = 1;
 
-                // index forward by 2 to skip the removed country name
-                l_WordIndex += 2;
-            } else {
-                
-                System.out.println("Invalid Command");
-                return "Invalid Command";
-            }
-        }
+			while (l_WordIndex < l_CommandSplitLength) {
+				// Check if the current word is "-remove" and there is at least one more word in
+				// the command
+				if (l_WordIndex + 1 < l_CommandSplitLength &&
+						l_CommandSplit[l_WordIndex].equalsIgnoreCase("-remove")) {
+					// Call method d_Map.removeCountry to remove a country
+					d_Map.removeCountry(l_CommandSplit[l_WordIndex + 1]);
 
-        
-        System.out.println("Countries removed successfully");
-        return "Countries removed successfully";
-    } else {
-        // If the first word is not "editcountry"
-        System.out.println("Invalid Command");
-        return "Invalid Command";
-    }
-}
-	
-	public String saveMap(String p_Command){
+					// index forward by 2 to skip the removed country name
+					l_WordIndex += 2;
+				} else {
+
+					System.out.println("Invalid Command");
+					return "Invalid Command";
+				}
+			}
+
+			System.out.println("Countries removed successfully");
+			return "Countries removed successfully";
+		} else {
+			// If the first word is not "editcountry"
+			System.out.println("Invalid Command");
+			return "Invalid Command";
+		}
+	}
+
+	public String saveMap(String p_Command) {
 		String l_FileName[] = p_Command.split(" ");
 		try {
 			d_Map.saveFile(l_FileName[1]);
@@ -166,7 +162,7 @@ public class MapController {
 		}
 		return "Map file loaded successfully!!";
 	}
-	
+
 	public void showMap() {
 		d_Map.showMap();
 	}
